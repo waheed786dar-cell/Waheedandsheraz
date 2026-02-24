fdd48aed28a598f343e11254f2915469f99bd106
waheed786dar-cell <waheed786dar@gmail.com>
2026-02-24 10:58:01 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10645916
+    "zoneId": 10647094
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
